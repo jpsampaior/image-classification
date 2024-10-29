646f07f637a0585a78dfa4c7f51fbed40fdc0c9c
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2024-10-29 12:05:04 -0400

    :sparkles: creating dataloaders and resnet

--- cifar10.py
+++ cifar10.py
@@ -1,12 +1,21 @@
-import torch
 import torchvision
 import torchvision.transforms as transforms
 from collections import defaultdict
+from torchvision.models import resnet18
+from torch.utils.data import DataLoader
+import torch.nn as nn
 
-tensor_transform = transforms.Compose([transforms.ToTensor()])
+# Prepare to use ResNet-18 (Resize and normalize)
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
 
-train_data = torchvision.datasets.CIFAR10(root="./data", train=True, download=True, transform=tensor_transform)
-val_data = torchvision.datasets.CIFAR10(root="./data", train=False, download=True, transform=tensor_transform)
+# Load CIFAR-10 dataset
+train_data = torchvision.datasets.CIFAR10(root="./data", train=True, download=True, transform=transform)
+val_data = torchvision.datasets.CIFAR10(root="./data", train=False, download=True, transform=transform)
+
 
 def filter_by_class_limit(dataset, class_limit):
     filtered_data = []
@@ -18,7 +27,6 @@ def filter_by_class_limit(dataset, class_limit):
         if class_counts[label] < class_limit:
             filtered_data.append((img, label))
             class_counts[label] += 1
-
         # If we achieved our goal in all classes, break the loop
         if all(count >= class_limit for count in class_counts.values()):
             break
@@ -26,9 +34,21 @@ def filter_by_class_limit(dataset, class_limit):
     return filtered_data
 
 
-# Filter images
+# Filter images to get the first 500 training images and 100 test images per class
 filtered_train_data = filter_by_class_limit(train_data, class_limit=500)
 filtered_val_data = filter_by_class_limit(val_data, class_limit=100)
 
+# Checking if the images were loaded as needed
 print("Number of training images:", len(filtered_train_data))
 print("Number of test images:", len(filtered_val_data))
+
+# Creating DataLoaders - Question: Why do that?
+train_loader = DataLoader(filtered_train_data, batch_size=32, shuffle=True)
+val_loader = DataLoader(filtered_val_data, batch_size=32, shuffle=False)
+
+# Load ResNet-18 and remove the last layer
+model = resnet18()
+model.fc = nn.Identity()  # Replace the last layer with an identity layer
+model.eval()  # Question: Why do that?
+
+
