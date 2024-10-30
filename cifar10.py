4da2225dcee09a7ee7531cf38953a369de2ded75
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2024-10-30 12:34:03 -0400

    :sparkles: extract features

--- cifar10.py
+++ cifar10.py
@@ -1,3 +1,4 @@
+import torch
 import torchvision
 import torchvision.transforms as transforms
 from collections import defaultdict
@@ -5,7 +6,6 @@ from torchvision.models import resnet18
 from torch.utils.data import DataLoader
 import torch.nn as nn
 
-# This is Alexis hi!
 
 # Prepare to use ResNet-18 (Resize and normalize)
 transform = transforms.Compose([
@@ -37,8 +37,8 @@ def filter_by_class_limit(dataset, class_limit):
 
 
 # Filter images to get the first 500 training images and 100 test images per class
-filtered_train_data = filter_by_class_limit(train_data, class_limit=500)
-filtered_val_data = filter_by_class_limit(val_data, class_limit=100)
+filtered_train_data = filter_by_class_limit(train_data, class_limit=10)
+filtered_val_data = filter_by_class_limit(val_data, class_limit=5)
 
 # Checking if the images were loaded as needed
 print("Number of training images:", len(filtered_train_data))
@@ -53,4 +53,27 @@ model = resnet18()
 model.fc = nn.Identity()  # Replace the last layer with an identity layer
 model.eval()  # Question: Why do that?
 
+# Question: Why do that?
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+
+# Feature extraction function
+def extract_features(data_loader):
+    features = []
+    # No Gradient to speed the process
+    with torch.no_grad():
+        for imgs, lbls in data_loader:
+            # Extract features using ResNet18 model
+            features.append(model(imgs))
+    return torch.cat(features)
+
+
+# Extracting features
+train_features = extract_features(train_loader)
+val_features = extract_features(val_loader)
+
+# Verify features extraction
+print("Training features shape:", train_features.shape)
+print("Validation features shape:", val_features.shape)
 
