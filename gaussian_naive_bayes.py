9f79710b2462d0d147c75d50181986eb2c412535
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2024-11-12 16:37:08 -0500

    :sparkles: naive bayes implementation

--- gaussian_naive_bayes.py
+++ gaussian_naive_bayes.py
@@ -19,3 +19,25 @@ class GaussianNaiveBayes:
             # número de amostras da classe / total de amostras
             self.class_priors[class_label] = class_samples.shape[0] / feature_vectors.shape[0]
 
+    def gaussian_density(self, class_label, feature_vector):
+        variance = self.class_variances[class_label]
+        mean = self.class_means[class_label]
+        exponent = np.exp(-((feature_vector - mean) ** 2) / (2 * variance))
+        return (1 / np.sqrt(2 * np.pi * variance)) * exponent
+
+    def predict(self, feature_vectors):
+        predicted_classes = []
+
+        for vector in feature_vectors:
+            class_scores = {}
+
+            for class_label in self.class_means:
+                # Using log of probabilities, just like in Assignment 2
+                log_prior = np.log(self.class_priors[class_label])
+                log_likelihood = np.sum(np.log(self.gaussian_density(class_label, vector)))
+                class_scores[class_label] = log_prior + log_likelihood
+
+            predicted_class = max(class_scores, key=class_scores.get)
+            predicted_classes.append(predicted_class)
+
+        return np.array(predicted_classes)
--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from feature_extractor import FeatureExtractor
+import numpy as np
+from gaussian_naive_bayes import GaussianNaiveBayes
 
 
 def main():
-    extractor = FeatureExtractor(train_class_limit=10, val_class_limit=5)
+    extractor = FeatureExtractor()
     train_features_pca, val_features_pca = extractor.process()
+
+    train_labels = np.array([label for _, label in extractor.filtered_train_data])
+    val_labels = np.array([label for _, label in extractor.filtered_val_data])
+
+    gnb = GaussianNaiveBayes()
+    gnb.train_model(train_features_pca, train_labels)
+
+    val_predictions = gnb.predict(val_features_pca)
+
+    accuracy = np.mean(val_predictions == val_labels)
+    print("Naive Bayes Accuracy:", accuracy)
 
 
 if __name__ == "__main__":
