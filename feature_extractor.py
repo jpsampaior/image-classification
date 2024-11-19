4b6e21ff29906268b07cb56c0278ac93c1dffacd
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2024-11-19 17:17:45 -0500

    better results visualization

--- feature_extractor.py
+++ feature_extractor.py
@@ -21,49 +21,66 @@ def filter_by_class_limit(dataset, class_limit):
 
 
 class FeatureExtractor:
-    def __init__(self, train_class_limit=500, val_class_limit=100, batch_size=32, pca_components=50):
+    def __init__(self, train_class_limit=500, test_class_limit=100, batch_size=32, pca_components=50):
         self.train_class_limit = train_class_limit
-        self.val_class_limit = val_class_limit
+        self.test_class_limit = test_class_limit
         self.batch_size = batch_size
         self.pca_components = pca_components
 
+        # Prepare to resize images to 224x224x3
         self.transform = transforms.Compose([
             transforms.Resize((224, 224)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
 
+        # Load dataset (CIFAR10) using the resize option
         self.train_data = torchvision.datasets.CIFAR10(root="./data", train=True, download=True,
                                                        transform=self.transform)
-        self.val_data = torchvision.datasets.CIFAR10(root="./data", train=False, download=True,
+        self.test_data = torchvision.datasets.CIFAR10(root="./data", train=False, download=True,
                                                      transform=self.transform)
 
+        # Filter the data to get 500 training images (50 of each class) and 100 test images (10 of each class)
+        print(f"\nFiltering data to get {train_class_limit*10} training images and {test_class_limit*10} testing images...")
         self.filtered_train_data = filter_by_class_limit(self.train_data, self.train_class_limit)
-        self.filtered_val_data = filter_by_class_limit(self.val_data, self.val_class_limit)
+        self.filtered_test_data = filter_by_class_limit(self.test_data, self.test_class_limit)
+        print("Number of training images:", len(self.filtered_train_data))
+        print("Number of test images:", len(self.filtered_test_data))
 
+        # Creating dataloaders
         self.train_loader = DataLoader(self.filtered_train_data, batch_size=self.batch_size, shuffle=True)
-        self.val_loader = DataLoader(self.filtered_val_data, batch_size=self.batch_size, shuffle=False)
+        self.test_loader = DataLoader(self.filtered_test_data, batch_size=self.batch_size, shuffle=False)
 
+        # Initializing the Resnet18 Model to extract the features vectors
         self.model = resnet18()
-        self.model.fc = nn.Identity()
+        self.model.fc = nn.Identity()  # Remove the last layer of Resnet18
         self.model.eval()
 
+    # Function used to extract the features
     def extract_features(self, data_loader):
         features = []
         with torch.no_grad():
             for imgs, _ in data_loader:
                 features.append(self.model(imgs))
-        return torch.cat(features)
+        return torch.cat(features)  # Concatenating the multiple batches
 
+    # Function used to apply the PCA to reduce the size of feature vectors from 512×1 50×1
     def apply_pca(self, features):
         pca = PCA(n_components=self.pca_components)
         return pca.fit_transform(features)
 
+    # Function that runs everything
     def process(self):
+        print("\nExtracting features using Resnet...")
         train_features = self.extract_features(self.train_loader)
-        val_features = self.extract_features(self.val_loader)
+        test_features = self.extract_features(self.test_loader)
+        print("Train features shape after extraction:", train_features.shape)
+        print("Test features shape after extraction:", test_features.shape)
 
+        print("\nReducing features with PCA...")
         train_features_pca = self.apply_pca(train_features)
-        val_features_pca = self.apply_pca(val_features)
+        test_features_pca = self.apply_pca(test_features)
+        print("Train features shape after PCA:", train_features_pca.shape)
+        print("Test features shape after PCA:", test_features_pca.shape)
 
-        return train_features_pca, val_features_pca
+        return train_features_pca, test_features_pca
--- main.py
+++ main.py
@@ -7,24 +7,26 @@ from sklearn.metrics import accuracy_score
 
 def main():
     extractor = FeatureExtractor()
-    train_features_pca, val_features_pca = extractor.process()
+    train_features_pca, test_features_pca = extractor.process()
 
     train_labels = np.array([label for _, label in extractor.filtered_train_data])
-    val_labels = np.array([label for _, label in extractor.filtered_val_data])
+    test_labels = np.array([label for _, label in extractor.filtered_test_data])
 
+    print("\nStarting the training process (Custom GNB)...")
     gnb = GaussianNaiveBayes()
     gnb.train_model(train_features_pca, train_labels)
 
-    val_predictions = gnb.predict(val_features_pca)
+    test_predictions = gnb.predict(test_features_pca)
 
-    accuracy = np.mean(val_predictions == val_labels)
-    print("Naive Bayes Accuracy:", accuracy)
+    accuracy = np.mean(test_predictions == test_labels)
+    print("Custom Naive Bayes Accuracy:", accuracy)
 
+    print("\n Starting the training process (Scikit GNB)...")
     sklearn_gnb = GaussianNB()
     sklearn_gnb.fit(train_features_pca, train_labels)
-    sklearn_val_predictions = sklearn_gnb.predict(val_features_pca)
+    sklearn_test_predictions = sklearn_gnb.predict(test_features_pca)
 
-    sklearn_accuracy = accuracy_score(val_labels, sklearn_val_predictions)
+    sklearn_accuracy = accuracy_score(test_labels, sklearn_test_predictions)
     print("Scikit-learn Naive Bayes Accuracy:", sklearn_accuracy)
 
 
