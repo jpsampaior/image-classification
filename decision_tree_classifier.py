b1ffa95237c0fb8f6b81fcef314dda8ef15d921a
AlexisOwOw <alexiisbernier01@gmail.com>
2024-11-19 17:33:07 -0500

    Create decision_tree_classifier.py

---
+++ decision_tree_classifier.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class DecitionTreeNode():
+    def __init__(self, feature_vector_index = None, threshold = None, go_left = None, go_right = None, value = None):
+        
+
+class DecitionTreeClassifier():
+    def __init__(self, max_depth = 50):
+        self.max_depth = max_depth
+        self.final_tree = None
+
+
+
+    
+
