9ce1aea9bb6b643a7b7b62511ac6d9f796c9abcd
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2024-11-23 23:41:27 -0500

    :sparkles: gnb changes and main menu

--- gaussian_naive_bayes.py
+++ gaussian_naive_bayes.py
@@ -3,27 +3,31 @@ import numpy as np
 
 class GaussianNaiveBayes:
     def __init__(self):
+        self.predictions = None
         self.class_means = {}
         self.class_variances = {}
         self.class_priors = {}
+        self.accuracy = None
 
     def train_model(self, feature_vectors, labels):
         unique_classes = np.unique(labels)
         for class_label in unique_classes:
-            # Filtra as amostras de que pertencem à classe atual
             class_samples = feature_vectors[labels == class_label]
 
             self.class_means[class_label] = class_samples.mean(axis=0)
-            self.class_variances[class_label] = class_samples.var(axis=0) + 1e-6
+            self.class_variances[class_label] = class_samples.var(axis=0)
 
-            # número de amostras da classe / total de amostras
             self.class_priors[class_label] = class_samples.shape[0] / feature_vectors.shape[0]
 
     def gaussian_density(self, class_label, feature_vector):
-        variance = self.class_variances[class_label]
         mean = self.class_means[class_label]
-        exponent = np.exp(-((feature_vector - mean) ** 2) / (2 * variance))
-        return (1 / np.sqrt(2 * np.pi * variance)) * exponent
+        variance = self.class_variances[class_label]
+        std_dev = np.sqrt(variance)
+
+        exponent = np.exp(-0.5 * ((feature_vector - mean) / std_dev) ** 2)
+        density = (1 / (std_dev * np.sqrt(2 * np.pi))) * exponent
+
+        return density
 
     def predict(self, feature_vectors):
         predicted_classes = []
@@ -32,12 +36,20 @@ class GaussianNaiveBayes:
             class_scores = {}
 
             for class_label in self.class_means:
-                # Using log of probabilities, just like in Assignment 2
                 log_prior = np.log(self.class_priors[class_label])
+
                 log_likelihood = np.sum(np.log(self.gaussian_density(class_label, vector)))
+
                 class_scores[class_label] = log_prior + log_likelihood
 
             predicted_class = max(class_scores, key=class_scores.get)
             predicted_classes.append(predicted_class)
 
-        return np.array(predicted_classes)
+        self.predictions = np.array(predicted_classes)
+
+        return self.predictions
+
+    def get_accuracy(self, labels):
+        self.accuracy = np.mean(self.predictions == labels)
+
+        return self.accuracy
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from sklearn.tree import DecisionTreeClassifier
-
 from decision_tree_classifier import CustomDTC
 from feature_extractor import FeatureExtractor
 import numpy as np
@@ -11,6 +10,100 @@ import torch
 import time
 
 
+def train_model_choice(choice, train_features_pca, train_labels, test_features_pca, test_labels):
+    if choice == '1':
+        print("\nStarting the training process (Custom GNB)...")
+        gnb = GaussianNaiveBayes()
+        gnb.train_model(train_features_pca, train_labels)
+        gnb.predict(test_features_pca)
+        accuracy = gnb.get_accuracy(test_labels)
+        print("Custom Naive Bayes Accuracy:", accuracy)
+
+    elif choice == '2':
+        print("\nStarting the training process (Scikit GNB)...")
+        sklearn_gnb = GaussianNB()
+        sklearn_gnb.fit(train_features_pca, train_labels)
+        sklearn_test_predictions = sklearn_gnb.predict(test_features_pca)
+        sklearn_accuracy = accuracy_score(test_labels, sklearn_test_predictions)
+        print("Scikit-learn Naive Bayes Accuracy:", sklearn_accuracy)
+
+    elif choice == '3':
+        print("\nStarting the training process (Custom DTC)...")
+        dtc = CustomDTC()
+        dtc.train_model(train_features_pca, train_labels)
+        test_predictions_dtc = dtc.predict(test_features_pca)
+        accuracy = np.mean(test_predictions_dtc == test_labels)
+        print("Custom Decision Tree Classifier Accuracy:", accuracy)
+
+    elif choice == '4':
+        print("\nStarting the training process (Scikit DTC)...")
+        sklearn_dtc = DecisionTreeClassifier()
+        sklearn_dtc.fit(train_features_pca, train_labels)
+        sklearn_dtc_test_predictions = sklearn_dtc.predict(test_features_pca)
+        sklearn_dtc_accuracy = accuracy_score(test_labels, sklearn_dtc_test_predictions)
+        print("Scikit-learn Decision Tree Accuracy: ", sklearn_dtc_accuracy)
+
+    elif choice == '5':
+        start_time = time.time()
+        print("\nStarting the training process (MLP default layers config)...")
+        mlp = MultiLayerPerceptron()
+        mlp.train_model(torch.tensor(train_features_pca, dtype=torch.float32),
+                        torch.tensor(train_labels, dtype=torch.long))
+        mlp_accuracy = mlp.predict(torch.tensor(test_features_pca, dtype=torch.float32),
+                                   torch.tensor(test_labels, dtype=torch.long))
+        end_time = time.time()
+        print("MLP Accuracy:", mlp_accuracy)
+        print(f"MLP Time (seconds): {end_time - start_time:.2f}")
+
+    elif choice == '6':
+        start_time = time.time()
+        print("\nStarting the training process (MLP with -1 layer)...")
+        mlp2 = MultiLayerPerceptron(depth=2)
+        mlp2.train_model(torch.tensor(train_features_pca, dtype=torch.float32),
+                         torch.tensor(train_labels, dtype=torch.long))
+        mlp_accuracy = mlp2.predict(torch.tensor(test_features_pca, dtype=torch.float32),
+                                    torch.tensor(test_labels, dtype=torch.long))
+        end_time = time.time()
+        print("MLP2 Accuracy:", mlp_accuracy)
+        print(f"MLP2 Time (seconds): {end_time - start_time:.2f}")
+
+    elif choice == '7':
+        start_time = time.time()
+        print("\nStarting the training process (MLP with +1 layer)...")
+        mlp3 = MultiLayerPerceptron(depth=4)
+        mlp3.train_model(torch.tensor(train_features_pca, dtype=torch.float32),
+                         torch.tensor(train_labels, dtype=torch.long))
+        mlp_accuracy = mlp3.predict(torch.tensor(test_features_pca, dtype=torch.float32),
+                                    torch.tensor(test_labels, dtype=torch.long))
+        end_time = time.time()
+        print("MLP3 Accuracy:", mlp_accuracy)
+        print(f"MLP3 Time (seconds): {end_time - start_time:.2f}")
+
+    elif choice == '8':
+        start_time = time.time()
+        print("\nStarting the training process (MLP with -256 layers size - 256 total)...")
+        mlp4 = MultiLayerPerceptron(hidden_layer_sizes=256)
+        mlp4.train_model(torch.tensor(train_features_pca, dtype=torch.float32),
+                         torch.tensor(train_labels, dtype=torch.long))
+        mlp_accuracy = mlp4.predict(torch.tensor(test_features_pca, dtype=torch.float32),
+                                    torch.tensor(test_labels, dtype=torch.long))
+        end_time = time.time()
+        print("MLP4 Accuracy:", mlp_accuracy)
+        print(f"MLP4 Time (seconds): {end_time - start_time:.2f}")
+
+    elif choice == '9':
+        start_time = time.time()
+        print("\nStarting the training process (MLP with +512 layers size - 1024 total)...")
+        mlp5 = MultiLayerPerceptron(hidden_layer_sizes=1024)
+        mlp5.train_model(torch.tensor(train_features_pca, dtype=torch.float32),
+                         torch.tensor(train_labels, dtype=torch.long))
+        mlp_accuracy = mlp5.predict(torch.tensor(test_features_pca, dtype=torch.float32),
+                                    torch.tensor(test_labels, dtype=torch.long))
+        end_time = time.time()
+        print("MLP5 Accuracy:", mlp_accuracy)
+        print(f"MLP5 Time (seconds): {end_time - start_time:.2f}")
+
+
 def main():
     extractor = FeatureExtractor()
     train_features_pca, train_labels, test_features_pca, test_labels = extractor.get_features_and_labels()
@@ -20,83 +113,30 @@ def main():
     train_labels = train_labels.detach().cpu().numpy()
     test_labels = test_labels.detach().cpu().numpy()
 
-    print("\nStarting the training process (Custom GNB)...")
-    gnb = GaussianNaiveBayes()
-    gnb.train_model(train_features_pca, train_labels)
-    test_predictions = gnb.predict(test_features_pca)
-    test_predictions = np.array(test_predictions)
-    accuracy = np.mean(test_predictions == test_labels)
-    print("Custom Naive Bayes Accuracy:", accuracy)
+    while True:
+        print("\nChoose a model to train and predict:")
+        print("1. Custom Gaussian Naive Bayes (Custom GNB)")
+        print("2. Scikit-learn Gaussian Naive Bayes (SK-Learn GNB)")
+        print("3. Custom Decision Tree Classifier (Custom DTC)")
+        print("4. Scikit-learn Decision Tree Classifier (SK-Learn DTC)")
+        print("5. Multi Layer Perceptron (MLP) with default configuration")
+        print("6. Multi Layer Perceptron (MLP) with -1 layer")
+        print("7. Multi Layer Perceptron (MLP) with +1 layer")
+        print("8. Multi Layer Perceptron (MLP) with -256 layer size (total 256)")
+        print("9. Multi Layer Perceptron (MLP) with +512 layer size (total 1024)")
+        print("0. Exit")
 
-    print("\nStarting the training process (Scikit GNB)...")
-    sklearn_gnb = GaussianNB()
-    sklearn_gnb.fit(train_features_pca, train_labels)
-    sklearn_test_predictions = sklearn_gnb.predict(test_features_pca)
-    sklearn_accuracy = accuracy_score(test_labels, sklearn_test_predictions)
-    print("Scikit-learn Naive Bayes Accuracy:", sklearn_accuracy)
+        choice = input("\nEnter the number of the model you want to train: ")
+        if choice == '0':
+            print("\nExiting...")
+            break
 
-    # print("\nStarting the training process (Custom DTC)...")
-    # dtc = CustomDTC()
-    # dtc.train_model(train_features_pca, train_labels)
-    # test_predictions_dtc = dtc.predict(test_features_pca)
-    # test_predictions_dtc = np.array(test_predictions_dtc)
-    # accuracy = np.mean(test_predictions_dtc == test_labels)
-    # print("Custom Decision Tree Classifier:", accuracy)
+        train_model_choice(choice, train_features_pca, train_labels, test_features_pca, test_labels)
 
-    print("\nStarting the training process (Scikit DTC)...")
-    sklearn_dtc = DecisionTreeClassifier()
-    sklearn_dtc.fit(train_features_pca, train_labels)
-    sklearn_dtc_test_predictions = sklearn_dtc.predict(test_features_pca)
-    sklearn_dtc_accuracy = accuracy_score(test_labels, sklearn_dtc_test_predictions)
-    print("Scikit-learn Decision Tree Accuracy: ", sklearn_dtc_accuracy)
-
-    start_time = time.time()
-    print("\nStarting the training process (MLP default layers config)...")
-    mlp = MultiLayerPerceptron()
-    mlp.train_model(torch.tensor(train_features_pca, dtype=torch.float32), torch.tensor(train_labels, dtype=torch.long))
-    mlp_accuracy = mlp.predict(torch.tensor(test_features_pca, dtype=torch.float32),
-                               torch.tensor(test_labels, dtype=torch.long))
-    end_time = time.time()
-    print("MLP Accuracy:", mlp_accuracy)
-    print(f"MLP Time (seconds): {end_time - start_time:.2f}")
-
-    print("\nStarting the training process (MLP with -1 layer)...")
-    mlp2 = MultiLayerPerceptron(depth=2)
-    mlp2.train_model(torch.tensor(train_features_pca, dtype=torch.float32),
-                     torch.tensor(train_labels, dtype=torch.long))
-    mlp_accuracy = mlp2.predict(torch.tensor(test_features_pca, dtype=torch.float32),
-                                torch.tensor(test_labels, dtype=torch.long))
-    print("MLP2 Accuracy:", mlp_accuracy)
-
-    print("\nStarting the training process (MLP with +1 layer)...")
-    mlp3 = MultiLayerPerceptron(depth=4)
-    mlp3.train_model(torch.tensor(train_features_pca, dtype=torch.float32),
-                     torch.tensor(train_labels, dtype=torch.long))
-    mlp_accuracy = mlp3.predict(torch.tensor(test_features_pca, dtype=torch.float32),
-                                torch.tensor(test_labels, dtype=torch.long))
-    print("MLP3 Accuracy:", mlp_accuracy)
-
-    start_time = time.time()
-    print("\nStarting the training process (MLP with -256 layers size - 256 total)...")
-    mlp4 = MultiLayerPerceptron(hidden_layer_sizes=256)
-    mlp4.train_model(torch.tensor(train_features_pca, dtype=torch.float32),
-                     torch.tensor(train_labels, dtype=torch.long))
-    mlp_accuracy = mlp4.predict(torch.tensor(test_features_pca, dtype=torch.float32),
-                                torch.tensor(test_labels, dtype=torch.long))
-    end_time = time.time()
-    print("MLP4 Accuracy:", mlp_accuracy)
-    print(f"MLP4 Time (seconds): {end_time - start_time:.2f}")
-
-    start_time = time.time()
-    print("\nStarting the training process (MLP with +512 layers size - 1024 total)...")
-    mlp5 = MultiLayerPerceptron(hidden_layer_sizes=1024)
-    mlp5.train_model(torch.tensor(train_features_pca, dtype=torch.float32),
-                     torch.tensor(train_labels, dtype=torch.long))
-    mlp_accuracy = mlp5.predict(torch.tensor(test_features_pca, dtype=torch.float32),
-                                torch.tensor(test_labels, dtype=torch.long))
-    end_time = time.time()
-    print("MLP5 Accuracy:", mlp_accuracy)
-    print(f"MLP5 Time (seconds): {end_time - start_time:.2f}")
+        again = input("\nDo you want to train another model? (y/n): ")
+        if again.lower() != 'y':
+            print("\nExiting...")
+            break
 
 
 if __name__ == "__main__":
