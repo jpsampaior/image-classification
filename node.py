3c06ffa05e9ab2fef14ec84818f28f32524653cc
AlexisOwOw <alexiisbernier01@gmail.com>
2024-11-24 17:41:58 -0500

    added comments to node

--- node.py
+++ node.py
@@ -1,10 +1,14 @@
+'''
+Node class which serves as the data stucture containing the left and right subtrees, decision function (gini), as well as labels.
+'''
 class Node:
     def __init__(self, left, right, decision_function, label=None):
         self.left = left
         self.right = right
         self.decision_function = decision_function
         self.label = label
-
+    
+    # Determines label given a feature vector.
     def decide(self, feature):
         if self.label is not None:
             return self.label
