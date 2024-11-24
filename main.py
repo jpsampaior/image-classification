8d4800ea980aba81d72d6939fcbe7cac8188b911
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2024-11-24 14:15:49 -0500

    :art: mlp improve code quality

--- main.py
+++ main.py
@@ -48,10 +48,9 @@ def train_model_choice(choice, train_features_pca, train_labels, test_features_p
         start_time = time.time()
         print("\nStarting the training process (MLP default layers config)...")
         mlp = MultiLayerPerceptron()
-        mlp.train_model(torch.tensor(train_features_pca, dtype=torch.float32),
-                        torch.tensor(train_labels, dtype=torch.long))
-        mlp_accuracy = mlp.predict(torch.tensor(test_features_pca, dtype=torch.float32),
-                                   torch.tensor(test_labels, dtype=torch.long))
+        mlp.train_model(train_features_pca, train_labels)
+        mlp.predict(test_features_pca)
+        mlp_accuracy = mlp.get_accuracy(test_labels)
         end_time = time.time()
         print("MLP Accuracy:", mlp_accuracy)
         print(f"MLP Time (seconds): {end_time - start_time:.2f}")
@@ -60,10 +59,9 @@ def train_model_choice(choice, train_features_pca, train_labels, test_features_p
         start_time = time.time()
         print("\nStarting the training process (MLP with -1 layer)...")
         mlp2 = MultiLayerPerceptron(depth=2)
-        mlp2.train_model(torch.tensor(train_features_pca, dtype=torch.float32),
-                         torch.tensor(train_labels, dtype=torch.long))
-        mlp_accuracy = mlp2.predict(torch.tensor(test_features_pca, dtype=torch.float32),
-                                    torch.tensor(test_labels, dtype=torch.long))
+        mlp2.train_model(train_features_pca, train_labels)
+        mlp2.predict(test_features_pca)
+        mlp_accuracy = mlp2.get_accuracy(test_labels)
         end_time = time.time()
         print("MLP2 Accuracy:", mlp_accuracy)
         print(f"MLP2 Time (seconds): {end_time - start_time:.2f}")
@@ -72,10 +70,9 @@ def train_model_choice(choice, train_features_pca, train_labels, test_features_p
         start_time = time.time()
         print("\nStarting the training process (MLP with +1 layer)...")
         mlp3 = MultiLayerPerceptron(depth=4)
-        mlp3.train_model(torch.tensor(train_features_pca, dtype=torch.float32),
-                         torch.tensor(train_labels, dtype=torch.long))
-        mlp_accuracy = mlp3.predict(torch.tensor(test_features_pca, dtype=torch.float32),
-                                    torch.tensor(test_labels, dtype=torch.long))
+        mlp3.train_model(train_features_pca, train_labels)
+        mlp3.predict(test_features_pca)
+        mlp_accuracy = mlp3.get_accuracy(test_labels)
         end_time = time.time()
         print("MLP3 Accuracy:", mlp_accuracy)
         print(f"MLP3 Time (seconds): {end_time - start_time:.2f}")
@@ -84,10 +81,9 @@ def train_model_choice(choice, train_features_pca, train_labels, test_features_p
         start_time = time.time()
         print("\nStarting the training process (MLP with -256 layers size - 256 total)...")
         mlp4 = MultiLayerPerceptron(hidden_layer_sizes=256)
-        mlp4.train_model(torch.tensor(train_features_pca, dtype=torch.float32),
-                         torch.tensor(train_labels, dtype=torch.long))
-        mlp_accuracy = mlp4.predict(torch.tensor(test_features_pca, dtype=torch.float32),
-                                    torch.tensor(test_labels, dtype=torch.long))
+        mlp4.train_model(train_features_pca, train_labels)
+        mlp4.predict(test_features_pca)
+        mlp_accuracy = mlp4.get_accuracy(test_labels)
         end_time = time.time()
         print("MLP4 Accuracy:", mlp_accuracy)
         print(f"MLP4 Time (seconds): {end_time - start_time:.2f}")
@@ -96,10 +92,9 @@ def train_model_choice(choice, train_features_pca, train_labels, test_features_p
         start_time = time.time()
         print("\nStarting the training process (MLP with +512 layers size - 1024 total)...")
         mlp5 = MultiLayerPerceptron(hidden_layer_sizes=1024)
-        mlp5.train_model(torch.tensor(train_features_pca, dtype=torch.float32),
-                         torch.tensor(train_labels, dtype=torch.long))
-        mlp_accuracy = mlp5.predict(torch.tensor(test_features_pca, dtype=torch.float32),
-                                    torch.tensor(test_labels, dtype=torch.long))
+        mlp5.train_model(train_features_pca, train_labels)
+        mlp5.predict(test_features_pca)
+        mlp_accuracy = mlp5.get_accuracy(test_labels)
         end_time = time.time()
         print("MLP5 Accuracy:", mlp_accuracy)
         print(f"MLP5 Time (seconds): {end_time - start_time:.2f}")
--- multi_layer_perceptron.py
+++ multi_layer_perceptron.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -6,11 +7,10 @@ import torch.optim as optim
 class MultiLayerPerceptron(nn.Module):
     def __init__(self, features_size=50, hidden_layer_sizes=512, output_size=10, depth=3):
         super(MultiLayerPerceptron, self).__init__()
-
-        # Add first layer - Linear(50, 512)- ReLU
+        # Add first layer - Linear(50, 512) - ReLU
         layers = [nn.Linear(features_size, hidden_layer_sizes), nn.ReLU()]
 
-        # Add middle layers - Linear(512, 512)- BatchNorm(512)- ReLU
+        # Add middle layers - Linear(512, 512) - BatchNorm(512) - ReLU
         for i in range(depth - 2):
             layers.append(nn.Linear(hidden_layer_sizes, hidden_layer_sizes))
             layers.append(nn.BatchNorm1d(hidden_layer_sizes))
@@ -19,35 +19,44 @@ class MultiLayerPerceptron(nn.Module):
         # Add last layer - Linear(512, 10)
         layers.append(nn.Linear(hidden_layer_sizes, output_size))
 
+        # Connecting all the layers
         self.network = nn.Sequential(*layers)
+        self.predicted = None
+        self.accuracy = None
 
     def forward(self, x):
         return self.network(x)
 
-    def train_model(self, train_features, train_labels, num_epochs=10, learning_rate=0.01, momentum=0.9):
+    def train_model(self, train_features, train_labels, num_epochs=10, momentum=0.9):
+        if not isinstance(train_features, torch.Tensor):
+            train_features = torch.tensor(train_features, dtype=torch.float32)
+        if not isinstance(train_labels, torch.Tensor):
+            train_labels = torch.tensor(train_labels, dtype=torch.long)
+
+        # Using the cross-entropy loss and sgd optimizer with the desired momentum
         criterion = nn.CrossEntropyLoss()
-        optimizer = optim.SGD(self.parameters(), lr=learning_rate, momentum=momentum)
+        optimizer = optim.SGD(self.parameters(), momentum=momentum)
 
         for epoch in range(num_epochs):
-            running_loss = 0.0
-
             optimizer.zero_grad()
             outputs = self(train_features)
             loss = criterion(outputs, train_labels)
             loss.backward()
             optimizer.step()
 
-            running_loss += loss.item()
-
-    def predict(self, test_features, test_labels):
-        correct = 0
-        total = 0
+    def predict(self, test_features):
+        # Convert to tensors if needed
+        if not isinstance(test_features, torch.Tensor):
+            test_features = torch.tensor(test_features, dtype=torch.float32)
 
         with torch.no_grad():
             outputs = self(test_features)
-            _, predicted = torch.max(outputs.data, 1)
-            total += test_labels.size(0)
-            correct += (predicted == test_labels).sum().item()
+            outputs = outputs.numpy()
 
-        accuracy = correct / total
-        return accuracy
+            self.predicted = np.argmax(outputs, axis=1)
+
+        return self.predicted
+
+    def get_accuracy(self, labels):
+        self.accuracy = np.mean(self.predicted == labels)
+        return self.accuracy
