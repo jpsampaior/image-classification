43980ef5cf36a4a0eeaf0bb8b98b78bab401fbc5
AlexisOwOw <alexiisbernier01@gmail.com>
2024-11-25 07:00:27 +0000

    confusion matrix

--- evaluation.py
+++ evaluation.py
@@ -1,1 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
 
+def generate_confusion_matrix_sklearn(actual_labels, predicted_labels, model_name = None):
+    cm = confusion_matrix(actual_labels, predicted_labels)
+    display_cm = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"])
+    display_cm.plot()
+    if model_name:
+        plt.title(f"Confusion Matrix for {model_name}")
+    plt.show()
+
+def generate_confusion_matrix_custom(actual_labels, predicted_labels, model_name = None):
+    n_classes = len(np.unique(actual_labels))
+    result = np.zeros((n_classes, n_classes))
+
+    for i in range(len(actual_labels)):
+
+        result[actual_labels[i]][predicted_labels[i]] += 1
+
+    if model_name:
+        print(f"Confusion Matrix for {model_name}:")
+    
+    print("airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck")
+    
+    print(result)
+
+def generate_evaluation_table():
